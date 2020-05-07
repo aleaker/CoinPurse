2ee1a2d4763f2994c106c5e6634b2ac308a5de53
aleaker <alejoakerfeld@gmail.com>
2020-05-07 14:15:46 -0300

    first deploy

--- front/webpack.config.js
+++ front/webpack.config.js
@@ -3,7 +3,7 @@ module.exports = {
   entry: ["babel-polyfill", "./src/index.js"],
   output: {
     filename: "bundle.js",
-    path: __dirname + "./build"
+    path: __dirname + "/build"
   },
   resolve: {
     extensions: [".js", ".jsx"]

b0e1134431dd12cbe35ead10eb99a824706a3f1e
aleaker <alejoakerfeld@gmail.com>
2020-05-07 15:44:26 -0300

    first deploy

--- config/db.js
+++ config/db.js
@@ -12,9 +12,7 @@ require("dotenv").config();
 const devConfig = `postgres://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
 
  console.log("aaaaaaaaaaaaaaaaaaaa")
-const proConfig = {
-  connectionString: process.env.DATABASE_URL //heroku addon
-};
+const proConfig = process.env.DATABASE_URL //heroku addon
 
 const db = new S(
   process.env.NODE_ENV === "production" ? proConfig : devConfig,

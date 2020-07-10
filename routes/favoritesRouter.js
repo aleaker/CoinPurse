6a3b4d9543f7d7cf24466ca1416388bab3856577
aleaker <alejoakerfeld@gmail.com>
2020-07-09 21:29:02 -0300

    favorites route refactored with async functions

--- routes/favoritesRouter.js
+++ routes/favoritesRouter.js
@@ -2,37 +2,39 @@ const router = require("express").Router();
 const User = require("../models/user");
 const Favorite = require("../models/favorite");
 
-
 router.get("/fetchFavorites", async (req, res) => {
-	const favorites = Favorite.findAll({ where: { UserId: req.user.id } })
-	.then(favorites =>
-    res.send(favorites)
-  );
+  const favorites = await Favorite.findAll({ where: { UserId: req.user.id } });
+  if (!!favorites) {
+    res.send(favorites).status(200);
+  } else {
+    res.send({ errorMsg: "Your are not following any asset" }).status(204);
+  }
 });
 
-router.post("/addFavorite", (req, res) => {
-  Favorite.findOne({
-    where: { UserId: req.user.id, coinId: req.body.coinId }
-  }).then(exists => {
-    !!exists
-      ? res.send("Already following the asset")
-      : User.findByPk(req.user.id).then(user => {
-          Favorite.create(req.body)
-            .then(fav => user.addFavorite(fav))
-            .then(() => res.sendStatus(201));
-        });
+router.post("/addFavorite", async (req, res) => {
+  const favorite = await Favorite.findOne({
+    where: { UserId: req.user.id, coinId: req.body.coinId },
   });
+  if (!!favorite) {
+    res.send({ errorMsg: "Already following the asset" }).status(406);
+  } else {
+    const user = await User.findByPk(req.user.id);
+    const fav = await Favorite.create(req.body);
+    user.addFavorite(fav);
+    res.sendStatus(201);
+  }
 });
 
 router.delete("/deleteFavorite", async (req, res) => {
-  try{
-    let fav = await Favorite.findOne({ where: { UserId: req.user.id, coinId: req.body.coinId } })
+  try {
+    let fav = await Favorite.findOne({
+      where: { UserId: req.user.id, coinId: req.body.coinId },
+    });
     await fav.destroy();
     res.sendStatus(204);
-  }catch(e){
+  } catch (e) {
     console.log(e);
-}
-
+  }
 });
 
 module.exports = router;

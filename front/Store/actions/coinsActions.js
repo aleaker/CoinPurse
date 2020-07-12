12733a766bd026fcb42357dad81845d5034d6aac
aleaker <alejoakerfeld@gmail.com>
2020-07-12 18:12:44 -0300

    coinsActions with async/await

--- front/Store/actions/coinsActions.js
+++ front/Store/actions/coinsActions.js
@@ -6,19 +6,15 @@ export const setCoins = coins => dispatch =>
 export const setSearched = searched => dispatch =>
   dispatch({ type: "SET_SEARCHED", searched });
 
-export const fetchCoins = wanted => dispatch => {
-  (wanted
-    ? axios.get(`https://api.coincap.io/v2/assets?ids=${wanted.join(',')}`)
-    : axios
-        .get("https://api.coincap.io/v2/assets")
-        )
-        .then(res => res.data.data)
-        .then(coins => dispatch(setCoins(coins)));
+export const fetchCoins = wanted => async(dispatch) => {
+	const coins = await (wanted ? 
+		axios.get(`https://api.coincap.io/v2/assets?ids=${wanted.join(',')}`)
+		:
+		axios.get("https://api.coincap.io/v2/assets")).data.data;
+	dispatch(setCoins(coins));
 };
 
-export const fetchSearchedCoins = wanted => dispatch => {
-  axios
-    .get(`https://api.coincap.io/v2/assets?ids=${wanted}`)
-    .then(res => res.data.data)
-    .then(searched => dispatch(setSearched(searched)));
+export const fetchSearchedCoins = wanted => async(dispatch) => {
+  const searchedCoins = await (await axios.get(`https://api.coincap.io/v2/assets?ids=${wanted}`)).data.data;
+	dispatch(setSearched(searchedCoins));
 };
